efd83c325c89bed5e00c145456f165b08e81bf1d
xumingjie1658 <flippyxu@gmail.com>
2015-10-04 21:24:00 +0800

    add springmvc demo

---
+++ springmvc/maven-compiler-plugin.md
@@ -0,0 +1,1 @@
+maven- compiler-plugin的compile目标用来编译位于src/main/java/目录下的主源码，testCompile目标用来编译位于src/test/java/目录下的测试源码
---
+++ springmvc/testspringmvc/src/main/java/com/springmvc/test/controller/HelloWorldController.java
@@ -0,0 +1,23 @@
+package com.springmvc.test.controller;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by xumingjie on 15/10/4.
+ */
+public class HelloWorldController implements Controller {
+
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
+        ModelAndView mv = new ModelAndView();
+        //添加模型数据 可以是任意的POJO对象
+        mv.addObject("message", "Hello World!");
+        //设置逻辑视图名，视图解析器会根据该名字解析到具体的视图页面
+        mv.setViewName("hello");
+        return mv;
+    }
+}
